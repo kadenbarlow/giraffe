b659d80054141a51e6ef4cac7f1271a9535ab310
Kaden Barlow <kadenbarlow@gmail.com>
2024-09-25 07:43:19 -0600

    handle-insertions is almost completely working

--- src/components/text-input/hooks/use-controller/actions/handle-insertions.js
+++ src/components/text-input/hooks/use-controller/actions/handle-insertions.js
@@ -3,10 +3,8 @@ const FUNCTION_KEYS = [
   "rightArrow",
   "upArrow",
   "downArrow",
-  "return",
   "escape",
   "ctrl",
-  "shift",
   "tab",
   "backspace",
   "delete",
@@ -16,20 +14,22 @@ const FUNCTION_KEYS = [
 ]
 
 export default function handleInsertions(ctx) {
-  const { cursorOffset, focus, input, key, unformattedValue } = ctx
-  if (focus && input && FUNCTION_KEYS.every((pressed) => !key[pressed])) {
-    return {
-      ...ctx,
-      cursorOffset: {
-        ...cursorOffset,
-        unformattedXOffset: cursorOffset.unformattedXOffset + 1,
-      },
-      insertion: true,
-      unformattedValue:
-        unformattedValue.slice(0, cursorOffset.unformattedXOffset) +
-        input +
-        unformattedValue.slice(cursorOffset.unformattedXOffset),
-    }
+  const { cursorOffset, focus, input, key, multiline, unformattedValue } = ctx
+
+  if (!focus) return ctx
+  if (key.return && !multiline) return ctx
+  if (FUNCTION_KEYS.some((pressed) => key[pressed])) return ctx
+
+  return {
+    ...ctx,
+    cursorOffset: {
+      ...cursorOffset,
+      unformattedXOffset: cursorOffset.unformattedXOffset + 1,
+    },
+    insertion: true,
+    unformattedValue:
+      unformattedValue.slice(0, cursorOffset.unformattedXOffset) +
+      input +
+      unformattedValue.slice(cursorOffset.unformattedXOffset),
   }
-  return ctx
 }
--- src/components/text-input/hooks/use-controller/index.js
+++ src/components/text-input/hooks/use-controller/index.js
@@ -11,7 +11,7 @@ import updateFormattedValue from "./actions/update-formatted-value.js"
 import updateUnformattedValue from "./actions/update-unformatted-value.js"
 
 export default function useController(props) {
-  const { focus, syntax, syntaxTheme, value, width, ...inkProps } = props
+  const { focus, multiline, syntax, syntaxTheme, value, width, ...inkProps } = props
 
   const cursorOffset = useRef({
     formattedXOffset: 0,
@@ -22,6 +22,11 @@ export default function useController(props) {
   const unformattedValue = useRef(value)
   const [formattedValue, setFormattedValue] = useState(value)
 
+  // TODO: Handle text wrapping
+  // handle cursors across multilines
+  // handle up and down arrows
+  // handle backspaces
+  // handle copy and paste (where input > a single character)
   useInput(
     (input, key) =>
       pipe.sync(
@@ -40,6 +45,7 @@ export default function useController(props) {
         formattedValue: null,
         input,
         key,
+        multiline,
         setFormattedValue,
         syntax,
         syntaxTheme,
