1fd1cb488371040765f8e186729b42c69518737e
Kaden Barlow <kadenbarlow@gmail.com>
2024-10-16 15:48:53 -0600

    small bug fix

--- src/lib/open-content-in-editor/actions/open-tmp-file-in-editor.js
+++ src/lib/open-content-in-editor/actions/open-tmp-file-in-editor.js
@@ -7,10 +7,10 @@ export default function openTmpFileInEditor(ctx) {
     // First exit fullscreen mode so that you can focus and unfocus without weird ui issues
     writeToStdout("\x1b[?1049l").then(() => {
       const child = spawn(process.env.EDITOR, [tmpFilePath], { stdio: "inherit" })
-      child.on("close", () => {
-        resolve(ctx)
-        writeToStdout("\x1b[?1049h") // after closing enter fullscreen and redraw the app
-        writeToStdout("\u001B[?25l")
+      child.on("close", async () => {
+        await writeToStdout("\x1b[?1049h") // after closing enter fullscreen and redraw the app
+        await writeToStdout("\u001B[?25l")
+        return resolve(ctx)
       })
     })
   })
