afc7394cb3a6a68325333b4c8fe59e438a2032d5
Kaden Barlow <kadenbarlow@gmail.com>
2024-10-29 20:42:21 -0600

    fix open in editor functionality

--- src/components/text-input/hooks/use-controller/actions/handle-open-in-editor/actions/open-content-in-editor/actions/open-tmp-file-in-editor.js
+++ src/components/text-input/hooks/use-controller/actions/handle-open-in-editor/actions/open-content-in-editor/actions/open-tmp-file-in-editor.js
@@ -5,10 +5,10 @@ export default function openTmpFileInEditor(ctx) {
   const { tmpFilePath } = ctx
   return new Promise((resolve) => {
     // First exit fullscreen mode so that you can focus and unfocus without weird ui issues
-    writeToStdout("\x1b[?1049l").then(() => {
+    writeToStdout("\x1b[?1049h").then(() => {
       const child = spawn(process.env.EDITOR, [tmpFilePath], { stdio: "inherit" })
       child.on("close", async () => {
-        await writeToStdout("\x1b[?1049h") // after closing enter fullscreen
+        await writeToStdout("\x1b[?1049l") // after closing enter fullscreen
         await writeToStdout("\u001B[?25l") // makes the cursor invisible
         return resolve(ctx)
       })
